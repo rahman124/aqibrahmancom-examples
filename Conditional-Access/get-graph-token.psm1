be0145d2378c86b8704faed5093b077a0c1cee1a
Aqib Rahman <rahman124@live.com>
2022-11-26 19:11:36 +0000

    Conditional Access example

---
+++ Conditional-Access/get-graph-token.psm1
@@ -0,0 +1,38 @@
+function Get-GraphToken {
+
+    <#
+    .SYNOPSIS
+    Azure AD OAuth Application Token for Graph API
+    Get OAuth token for a AAD Application (returned as $token
+    #>
+
+    # Application (client) ID, tenant ID and secret
+    Param(
+        [parameter(Mandatory = $true)]
+        $clientId,
+        [parameter(Mandatory = $true)]
+        $tenantId,
+        [parameter(Mandatory = $true)]
+        $clientSecret
+    )
+    
+    # Construct URI
+    $uri = "https://login.microsoftonline.com/$tenantId/oauth2/v2.0/token"
+     
+    # Construct Body
+    $body = @{
+        client_id     = $clientId
+        scope         = "https://graph.microsoft.com/.default"
+        client_secret = $clientSecret
+        grant_type    = "client_credentials"
+    }
+     
+    # Get OAuth 2.0 Token
+    $tokenRequest = Invoke-WebRequest -Method Post -Uri $uri -ContentType "application/x-www-form-urlencoded" -Body $body -UseBasicParsing
+     
+    # Access Token
+    $token = ($tokenRequest.Content | ConvertFrom-Json).access_token
+    
+    #Returns token
+    return $token
+}
